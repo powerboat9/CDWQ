cba9616762c9a8deac2924e2aca586b0f72a8e0f
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-11 17:18:41 -0400

    Update background.lua

--- background.lua
+++ background.lua
@@ -1,2 +1,26 @@
-local env = {}
+local CDWQPass = {} --
+
+local env = {
+    CDWQ = {
+        start = function(file)
+            coroutine.yield("CDWQ", "START", file)
+        end,
+        save = function(file)
+            coroutine.yield("CDWQ", "SAVE", file)
+        end,
+        off = function()
+            coroutine.yield("CDWQ", "OFF")
+        end,
+        unbind = function()
+            coroutine.yield("CDWQ", "UNBIND")
+        end
+    }
+}
+setmetatable(env, {__index = _G})
+        
 local shell = coroutine.create(loadstring("rom/programs/shell", env))
+
+while true do
+    local yieldData = {coroutine.resume(shell)}
+    if yieldData[1] == "CDWQ
+    
